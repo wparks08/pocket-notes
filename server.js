a9223455614f1aeda742c6c48c89833b9af9ab99
Will Parks <wparks08@gmail.com>
2020-02-28 15:05:47 -0800

    initial files created

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+
+// App & variables
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware added here
+// - Web form and JSON handling
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// - Logging
+app.use(morgan("dev"));
+
+// Static assets for Heroku deploy
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// API Routes here
+
+// Route all other requests to React frontend
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`API Server running on port ${PORT}`);
+});
