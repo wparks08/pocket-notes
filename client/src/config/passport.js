ff96b0c14b756b14e23fc2d2601a0e517614b4b1
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-03-07 14:01:52 -0800

    edited server, added config and .env file, created model, still need to add routes, and registration/login form

---
+++ client/src/config/passport.js
@@ -0,0 +1,95 @@
+const bcrypt = require("bcryptjs");
+const BCRYPT_SALT_ROUNDS = 10;
+const jwtSecret = process.env;
+const db = require("./db");
+
+const passport = require("passport"),
+    localStrategy = require("passport-local").Strategy,
+    User = require(""),
+    JWTStrategy = require("passport-jwt").Strategy,
+    ExtractJWT = require("passport-jwt").ExtractJwt;
+
+passport.use(
+    "register",
+    new localStrategy(
+        {
+            usernameField: "username",
+            passwordField: "password",
+            session: false
+        },
+        (username, password, done) => {
+            try {
+                db.User.findOne({ username: username }).then(user => {
+                    if (user !== null) {
+                        console.log("username taken");
+                        return done(null, false, { message: "username taken" });
+                    } else {
+                        bcrypt.hash(password, BCRYPT_SALT_ROUNDS).then(hashedPassword => {
+                            db.User.create({ username, password: hashedPassword }).then(user => {
+                                console.log("user created");
+                                return done(null, user, { message: "user created" });
+                            });
+                        });
+                    }
+                });
+            } catch (err) {
+                done(err, { message: error });
+            }
+        }
+    )
+);
+passport.use(
+    "login",
+    new localStrategy(
+        {
+            usernameField: "username",
+            passwordField: "password",
+            session: false
+        },
+        (username, password, done) => {
+            try {
+                db.User.findOne({ username: username }).then(user => {
+                    if (user === null) {
+                        return done(null, false, { message: "username does not exist" });
+                    } else {
+                        bcrypt.compare(password, user.password).then(response => {
+                            if (response !== true) {
+                                console.log("passwords do not match");
+                                return done(null, false, { message: "passwords do not match" });
+                            } else {
+                                console.log("user found and authenticated");
+                                return done(null, user, { message: "user found and authenticated" });
+                            }
+                        });
+                    }
+                });
+            } catch (err) {
+                done(err);
+            }
+        }
+    )
+);
+const opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme("JWT"),
+    secretOrKey: jwtSecret.CLIENT_SECRET
+};
+passport.use(
+    "jwt",
+    new JWTStrategy(opts, (jwtPayload, done) => {
+        try {
+            db.User.findOne({
+                username: jwtPayload.id
+            }).then(user => {
+                if (user) {
+                    console.log("user found in db in passport");
+                    done(null, user);
+                } else {
+                    console.log("user not found in db")
+                    done(null, false);
+                }
+            });
+        } catch (err) {
+            done(err);
+        }
+    })
+);
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
+require(".config/passport");
 // App & variables
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+app.use(cors());
 // Middleware added here
 // - Web form and JSON handling
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // - Logging
 app.use(morgan("dev"));
+app.use(passport.initialize());
 
 // Static assets for Heroku deploy
 if (process.env.NODE_ENV === "production") {
