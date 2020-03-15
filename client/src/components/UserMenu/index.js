fd3a82febe88a15a246ce8d4bc6a1b3aeb19c3cd
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-03-15 14:29:11 -0700

    running into bug when attempting logout method, needs more work, toggling component based on isAuthenticated state. will work on associating notes with users while awaiting help

--- client/src/components/UserMenu/index.js
+++ client/src/components/UserMenu/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import { Menu } from "@material-ui/core";
 import MenuItem from "@material-ui/core/MenuItem";
 import Divider from "@material-ui/core/Divider";
+import LoginControls from "../LoginControls";
 
 function UserMenu() {
     const [anchorEl, setAnchorEl] = useState(null);
@@ -34,7 +35,7 @@ function UserMenu() {
                 <MenuItem>Profile</MenuItem>
                 <MenuItem>Settings</MenuItem>
                 <Divider />
-                <MenuItem>Sign Out</MenuItem>
+                <LoginControls />
             </Menu>
         </>
     );
