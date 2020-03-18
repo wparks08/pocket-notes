d4576ccb5b1f719c360b8396f42b69c7a355e301
Will Parks <wparks08@gmail.com>
2020-03-17 19:31:03 -0700

    finish implementing design, and implement routing

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Card, CardContent, Typography } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import heroBg from "../../assets/images/landingbg.jpg";
@@ -8,6 +8,11 @@ import AppBar from "@material-ui/core/AppBar";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
+import { Link } from "react-router-dom";
+import CreateIcon from "@material-ui/icons/Create";
+import CloudIcon from "@material-ui/icons/Cloud";
+import ScheduleIcon from "@material-ui/icons/Schedule";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles(theme => ({
     herobg: {
@@ -44,7 +49,24 @@ const useStyles = makeStyles(theme => ({
         height: "100%"
     },
     sidekick: {
-        minHeight: "360px"
+        height: "400px",
+        backgroundColor: "#eee"
+    },
+    showcaseCard: {
+        width: 300,
+        height: 300,
+        textAlign: "center"
+    },
+    showcaseIcon: {
+        color: "rgba(255,255,255,0.90)",
+        backgroundColor: "#002984",
+        padding: theme.spacing(1),
+        borderRadius: "50%",
+        marginTop: theme.spacing(2)
+    },
+    showcaseDivider: {
+        marginTop: theme.spacing(5),
+        marginBottom: theme.spacing(5)
     }
 }));
 
@@ -56,8 +78,12 @@ function Landing() {
             <AppBar className={(classes.toolbar, classes.transparentBackground)}>
                 <Toolbar>
                     <Box className={classes.spacer}>&nbsp;</Box>
-                    <Button className={classes.topbarBtn}>Sign Up</Button>
-                    <Button className={classes.topbarBtn}>Log In</Button>
+                    <Button component={Link} to="/register" className={classes.topbarBtn}>
+                        Sign Up
+                    </Button>
+                    <Button component={Link} to="/login" className={classes.topbarBtn}>
+                        Log In
+                    </Button>
                 </Toolbar>
             </AppBar>
             <Grid container className={classes.heroOverlay} justify="center" alignItems="center">
@@ -68,11 +94,34 @@ function Landing() {
                 </Grid>
             </Grid>
             <Grid container justify="center" alignItems="center" className={classes.sidekick}>
-                <Grid item md={6} className={classes.flexCenter}>
-                    <Typography variant="h3">Notes. In your pocket.</Typography>
+                <Grid item md={4} className={classes.flexCenter}>
+                    <Card className={classes.showcaseCard}>
+                        <CardContent>
+                            <CreateIcon fontSize="large" className={classes.showcaseIcon} />
+                            <Divider className={classes.showcaseDivider} />
+                            <Typography gutterBottom>Write anything. Anywhere. Anytime.</Typography>
+                        </CardContent>
+                    </Card>
                 </Grid>
-                <Grid item md={6} className={classes.flexCenter}>
-                    <Typography>Here is the app that makes it happen.</Typography>
+                <Grid item md={4} className={classes.flexCenter}>
+                    <Card className={classes.showcaseCard}>
+                        <CardContent>
+                            <CloudIcon fontSize="large" className={classes.showcaseIcon} />
+                            <Divider className={classes.showcaseDivider} />
+                            <Typography gutterBottom>
+                                Keep your notes safe in the cloud, and access them when you need them.
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item md={4} className={classes.flexCenter}>
+                    <Card className={classes.showcaseCard}>
+                        <CardContent>
+                            <ScheduleIcon fontSize="large" className={classes.showcaseIcon} />
+                            <Divider className={classes.showcaseDivider} />
+                            <Typography gutterBottom>Ready when you are. Just log in, and start writing.</Typography>
+                        </CardContent>
+                    </Card>
                 </Grid>
             </Grid>
         </Container>
